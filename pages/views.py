89670e52b19cd8eea646e5225324886deca3f7a1
Alice <ledetele@gmail.com>
2025-10-08 16:05:34 +0800

    Step 11 : Index Template

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from listings.models import Listing
 # Create your views here.
 
 def index(request):
     print(request)
     print(request.path)
-    return render(request,'pages/index.html')
+    listings = Listing.objects.filter(is_published=True)[:3]
+    context = {'listings': listings}
+    return render(request,'pages/index.html',context)
 
 def about(request):
     print(request.path)
